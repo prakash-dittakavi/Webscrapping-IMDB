f36a466f0aa262a474af3c43fbb92508f1bd5a83
prakash-dittakavi <47428353+prakash-dittakavi@users.noreply.github.com>
2019-03-18 16:07:17 -0400

    Update Webscrapping.R

--- Webscrapping.R
+++ Webscrapping.R
@@ -1,30 +1,43 @@
-# Webscrapping Twitter for a particular hashtag
 # install.packages("RSelenium")
 library(RSelenium)
 rd <- rsDriver(browser = "chrome")
 rem_dr <- rd[["client"]]
 
 
-url <- "https://catalyst.uc.edu"
+url <- "https://twitter.com/"
 rem_dr$navigate(url) # Navigate to login page
 
 ## Manually enter username and password here
-user <- rem_dr$findElement(using = "name", value = "j_username")$clickElement() # Click the "Authorize" button to continue logging in
-rem_dr$sendKeysToActiveElement(list("****")) #---- UserName
+user <- rem_dr$findElement(using = "name", value = "session[username_or_email]")$clickElement() # Click the "Authorize" button to continue logging in
+rem_dr$sendKeysToActiveElement(list("Prakash24411125"))
 
 ## Manually enter password here
-pwd <- rem_dr$findElement(using = "name", value = "j_password")$clickElement() # Click the "Authorize" button to continue logging in
-rem_dr$sendKeysToActiveElement(list("*****"))
+pwd <- rem_dr$findElement(using = "name", value = "session[password]")$clickElement() # Click the "Authorize" button to continue logging in
+rem_dr$sendKeysToActiveElement(list("****"))
 
 
-rem_dr$findElement(using = "name", value = "_eventId_proceed")$clickElement()
-rem_dr$findElement()
+rem_dr$findElement(using = "css", value = ".js-submit")$clickElement()
 
 
-rem_dr$findElement(using = "id", "win0divPTNUI_LAND_REC_GROUPLET$0")$clickElement()
+search <- rem_dr$findElement(using = "css", value = "#search-query")$clickElement() # Click the "Authorize" button to continue logging in
+rem_dr$sendKeysToActiveElement(list("#AvengersEndgame"))
 
-rem_dr$findElement(using = "id", value = "PTGP_STEP_DVW_PTGP_STEP_LABEL$1")$clickElement()
-rem_dr$findElement(using = "id", value = "PTGP_STEP_DVW_PTGP_STEP_LABEL$11$$1")$clickElement()
+subm <- rem_dr$findElement(using = "css", value = ".nav-search")$clickElement()
+
+Sys.sleep(10)
+
+t <- rem_dr$findElements(using = 'css', ".tweet-text")
+tweets=list()
+tweets=sapply(t, function(x){x$getElementText()})
+for(i in 1:6){
+  print(tweets[i])
+}
+
+tweets_sub <- unlist(tweets)
+
+View(data.frame(tweets_sub))
+
+     
 
 #PT_PAGETITLElbl
 
